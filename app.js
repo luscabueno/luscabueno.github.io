e202db3136a633b81b6cf18cb1803c044c660bcc
Lucas Bueno <lucas.bueno.siqueira@gmail.com>
2025-05-02 17:58:39 -0300

    Page updated.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// group--filter--s
+
 window.addEventListener('scroll', function() {
     let elemRead = document.getElementById("header_filter");
     let elemShow = document.getElementById("header_filter_s");
@@ -12,4 +14,26 @@ window.addEventListener('scroll', function() {
 
 function convertRemToPixels(rem) {    
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
+}
+
+// popover
+
+let popoverTrigger = document.getElementById("popover_trigger");
+let popover = document.getElementById("popover");
+
+popoverTrigger.addEventListener('mouseover', function() {
+    popover.style.display = "block";
+});
+
+popoverTrigger.addEventListener('mouseleave', function() {
+    popover.style.display = "none";
+});
+
+popoverTrigger.addEventListener('mousemove', (event) => {
+    positionPopover(event);
+});
+
+function positionPopover(event) {
+    popover.style.left = (event.clientX + 20) + 'px';
+    popover.style.top = (event.clientY + 20) + 'px';
 }
