baafb8b60db186c73702184cc2038928525d698d
luscabueno <lucas.bueno.siqueira@gmail.com>
2025-05-09 17:13:48 -0300

    Page updated.

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-// group--filter--s
+// group--filter--s (repurpose it to reduce group--filter top padding maybe?)
 
-window.addEventListener('scroll', function() {
-    let elemRead = document.getElementById("header_filter");
-    let elemShow = document.getElementById("header_filter_s");
-    let rect = elemRead.getBoundingClientRect();
+// window.addEventListener('scroll', function() {
+//     let elemRead = document.getElementById("header_filter");
+//     let elemShow = document.getElementById("header_filter_s");
+//     let rect = elemRead.getBoundingClientRect();
 
-    if (rect.bottom < convertRemToPixels(4)) {
-        elemShow.style.display = "flex";
-    } else {
-        elemShow.style.display = "none";
-    }
-})
+//     if (rect.bottom < convertRemToPixels(4)) {
+//         elemShow.style.display = "flex";
+//     } else {
+//         elemShow.style.display = "none";
+//     }
+// })
 
 function convertRemToPixels(rem) {    
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
