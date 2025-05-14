a403d36eb3e6e169d242a26033f360b26ee25ca4
Lucas Bueno <lucas.bueno.siqueira@gmail.com>
2025-05-14 14:51:39 -0300

    Page updated.

---
+++ octans/app.js
@@ -0,0 +1,12 @@
+// access element
+let lateralNav = document.getElementById("lateral_nav");
+
+// open lateral nav
+function lateralNavOpen() {
+    lateralNav.style.display = "flex";
+}
+
+// close lateral nav
+function lateralNavClose() {
+    lateralNav.style.display = "none";
+}
