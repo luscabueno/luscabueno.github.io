05a7fa26e8511db454f37cce11fce436d725346d
Lucas Bueno <lucas.bueno.siqueira@gmail.com>
2025-05-20 01:02:12 -0300

    Page updated.

--- octans/app.js
+++ octans/app.js
@@ -13,3 +13,26 @@ function lateralNavOpen() {
 function lateralNavClose() {
     lateralNav.style.display = "none";
 }
+
+// close .select
+
+let select = document.querySelector(".select"),
+selectedValue = document.getElementById("selected_value"),
+inputsOption = document.querySelectorAll(".select--item input");
+
+inputsOption.forEach(input => {
+    input.addEventListener('click', event => {
+        // obtain input name
+        const selectName = input.name;
+
+        const selectValue = document.getElementById(`select_${selectName}_value`);
+        selectValue.innerHTML = input.dataset.label
+
+        const isMouseOrTouch =
+        event.pointerType == "mouse" ||
+        event.pointerType == "touch";
+
+        const selectExpand = document.getElementById(`select_${selectName}_expand`);
+        isMouseOrTouch && selectExpand.click();
+    })
+})
