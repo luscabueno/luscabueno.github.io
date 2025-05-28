af4224f0958b09969e467b2281fb71a50efaaca1
luscabueno <lucas.bueno.siqueira@gmail.com>
2025-05-28 14:52:05 -0300

    Page updated.

--- pyxis/app.js
+++ pyxis/app.js
@@ -1,8 +1,9 @@
-// lucide icons
-lucide.createIcons();
+// icon libraries
+    // lucide icons
+    lucide.createIcons();
 
-// feather icons
-feather.replace();
+    // feather icons
+    feather.replace();
 
 // open .select
 selectButtons = document.querySelectorAll('.select--button button');
@@ -10,7 +11,7 @@ selectButtons = document.querySelectorAll('.select--button button');
 selectButtons.forEach(button => {
     button.addEventListener('click', event => {
         // close other .select
-        const selectLists = document.getElementsByClassName('group--select--item');
+        const selectLists = document.getElementsByClassName('select--list');
         for (i = 0; i < selectLists.length; i++) {
             if (selectLists[i].classList.contains('show')) {
                 selectLists[i].classList.remove('show');
@@ -20,7 +21,7 @@ selectButtons.forEach(button => {
         // obtain .select name
         const selectName = button.id.split("_")[2];
 
-        // open .group--select--item
+        // open .select--list
         const selectList = document.getElementById(`select_list_${selectName}`);
         if (selectList.classList.contains('show')) {
             selectList.classList.remove('show')
@@ -42,7 +43,7 @@ selectItemInputs.forEach(input => {
         const selectValue = document.getElementById(`select_value_${selectName}`);
         selectValue.innerHTML = input.dataset.label
 
-        // close .group--select--item
+        // close .select--list
         const selectList = document.getElementById(`select_list_${selectName}`);
         selectList.classList.remove('show');
     })
@@ -50,7 +51,7 @@ selectItemInputs.forEach(input => {
 
 // cancel .select
 window.addEventListener('click', event => {
-    const selectLists = document.getElementsByClassName('group--select--item');
+    const selectLists = document.getElementsByClassName('select--list');
 
     if (!event.target.closest('.select--button')) {
         for (i = 0; i < selectLists.length; i++) {
@@ -62,3 +63,54 @@ window.addEventListener('click', event => {
 })
 
 // open .overlay and .popup
+popupButtons = document.querySelectorAll('.popup--button button');
+
+popupButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        // obtain .popup name
+        const popupName = button.id.split("_")[2];
+
+        // open .overlay
+        const overlay = document.getElementById('overlay');
+        overlay.classList.add('show');
+
+        // open .popup
+        const popup = document.getElementById(`popup_${popupName}`);
+        popup.classList.add('show');
+    })
+})
+
+// close .overlay and .popup
+popupButtonsClose = document.querySelectorAll('.popup--button--close button');
+
+popupButtonsClose.forEach(button => {
+    button.addEventListener('click', event => {
+        const popup = button.closest('.popup');
+        popup.classList.remove('show');
+
+        const overlay = document.getElementById('overlay');
+        overlay.classList.remove('show');
+    })
+})
+
+// switch .pop--body
+tabButtons = document.querySelectorAll('.tabsheet--button button');
+
+tabButtons.forEach(button => {
+    button.addEventListener('click', event => {
+        // close other .popup--body
+        const popupBodies = document.getElementsByClassName('popup--body');
+        for (i = 0; i < popupBodies.length; i++) {
+            if (popupBodies[i].classList.contains('show')) {
+                popupBodies[i].classList.remove('show');
+            }
+        }
+
+        // obtain .popup--body name
+        const popupBodyName = button.id.split('_')[2];
+
+        // open .popup--body
+        const popupBody = document.getElementById(`popup_body_${popupBodyName}`);
+        popupBody.classList.add('show');
+    })
+})
