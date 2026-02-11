5cc3f0c69e29abea5e5c694a3b9fbf04f9225b44
Lucas Bueno <lucas.bueno.siqueira@gmail.com>
2026-02-10 22:13:33 -0300

    Page updated.

---
+++ portfolio/app.js
@@ -0,0 +1,6 @@
+const dialog = document.querySelector(".test-dialog");
+const showDialog = document.querySelector(".show-test-dialog");
+
+showDialog.addEventListener("click", () => {
+    dialog.showModal();
+})
