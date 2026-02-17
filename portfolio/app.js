2e0ebe9a1eb0e539ba1124215e6a9692e3957936
Lucas Bueno <lucas.bueno.siqueira@gmail.com>
2026-02-17 18:17:35 -0300

    Page updated.

--- portfolio/app.js
+++ portfolio/app.js
@@ -1,11 +1,40 @@
-const dialog = document.querySelector("dialog.project");
-const showDialog = document.querySelector(".show-test-dialog");
-const closeDialog = document.querySelector(".close-test-dialog");
+// dialog
 
-showDialog.addEventListener("click", () => {
-    dialog.showModal();
+const dialog = document.querySelector('dialog.project');
+const showDialog = document.querySelectorAll('.show-dialog');
+const closeDialog = document.querySelectorAll('.close-dialog');
+
+showDialog.forEach(element => {
+    element.addEventListener('click', () => {
+        const projectName = element.closest('.card').id;
+        const projectGallery = dialog.querySelector('.gallery');
+
+        const projectImages = [
+            'img/projects/aquece-bem/aquece-bem_1.jpg',
+            'img/projects/aquece-bem/aquece-bem_2.jpg',
+            'img/projects/aquece-bem/aquece-bem_3.jpg',
+            'img/projects/aquece-bem/aquece-bem_4.jpg'
+        ];
+
+        // clear previous
+        projectGallery.replaceChildren();
+
+        // show current
+        projectImages.forEach(element => {
+            const img = document.createElement('img');
+            img.src = element;
+
+            projectGallery.append(img);
+        })
+
+        // show
+        dialog.showModal();
+    })
 })
 
-closeDialog.addEventListener("click", () => {
-    dialog.close();
+closeDialog.forEach(element => {
+    element.addEventListener('click', () => {
+        const dialog = element.closest('dialog');
+        dialog.close();
+    })
 })
